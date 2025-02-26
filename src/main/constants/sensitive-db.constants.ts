57361fb534fc7d12f6669a0ba387f34a287ddfa6
Nicolas <nicolas.gigou@gmail.com>
2025-02-26 16:33:37 +0100

    chore: update doc to warn about credentials caveat

--- src/main/constants/sensitive-db.constants.ts
+++ src/main/constants/sensitive-db.constants.ts
@@ -1,3 +1,8 @@
+// =================================================================================================
+// DANGER ZONE: This file contains the sensitive data for the database connection
+// Your modifications won't be pushed to the repository until you manually add them
+// This file is ignored by the .gitignore file
+// =================================================================================================
 const firebaseConfig = {
   apiKey: '',
   authDomain: '',
